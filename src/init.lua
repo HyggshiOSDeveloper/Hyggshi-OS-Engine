708e1944aa5582a194c872fbbc13f9bf92140b95
Hyggshi OS <tfhy5321@gmail.com>
2025-10-05 07:50:09 +0700

    Create init.lua

---
+++ src/init.lua
@@ -0,0 +1,17 @@
+-- init.lua
+local Engine = {}
+
+local Boot = require(script.Core.Boot)
+local UIManager = require(script.Core.UIManager)
+
+function Engine:Boot(osName)
+	print(osName .. " is starting...")
+	Boot:Init()
+	UIManager:Init()
+end
+
+function Engine:Shutdown()
+	print("Shutting down Hyggshi OS Engine...")
+end
+
+return Engine
